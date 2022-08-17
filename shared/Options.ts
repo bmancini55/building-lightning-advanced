cd68c86c43cbc723424a5656cfaf3c92dfdeed22
Brian Mancini <bmancini@gmail.com>
2022-08-17 13:14:05 -0400

    make config values optional

--- shared/Options.ts
+++ shared/Options.ts
@@ -14,9 +14,22 @@ export class Options {
         const lndRestHost = process.env.LND_REST_HOST;
         const lndRpcHost = process.env.LND_RPC_HOST;
         const lndCert = await fs.readFile(process.env.LND_CERT_PATH);
-        const lndAdminMacaroon = await fs.readFile(process.env.LND_ADMIN_MACAROON_PATH);
-        const lndInvoiceMacaroon = await fs.readFile(process.env.LND_INVOICE_MACAROON_PATH);
-        const lndReadonlyMacaroon = await fs.readFile(process.env.LND_READONLY_MACAROON_PATH);
+
+        let lndAdminMacaroon: Buffer;
+        if (process.env.LND_ADMIN_MACAROON_PATH) {
+            lndAdminMacaroon = await fs.readFile(process.env.LND_ADMIN_MACAROON_PATH);
+        }
+
+        let lndInvoiceMacaroon: Buffer;
+        if (process.env.LND_INVOICE_MACAROON_PATH) {
+            lndInvoiceMacaroon = await fs.readFile(process.env.LND_INVOICE_MACAROON_PATH);
+        }
+
+        let lndReadonlyMacaroon: Buffer;
+        if (process.env.LND_READONLY_MACAROON_PATH) {
+            lndReadonlyMacaroon = await fs.readFile(process.env.LND_READONLY_MACAROON_PATH);
+        }
+
         return new Options(
             port,
             lndRestHost,
